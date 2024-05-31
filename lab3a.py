d29cb4d04f77f36a196bf6f213209ca49f18442b
Bishwash BC <bb-c1@Bishwash>
2024-05-31 13:51:34 -0400

    added lab3a

---
+++ lab3a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# return_text_value() function
+# Author ID: [bb-c1]
+
+def return_text_value():
+    return "Good Morning Terry"
+
+# return_number_value() function
+
+def return_number_value():
+    return 15
+
+
+# Main Program
+
+
+if __name__ == '__main__':
+    import lab3a
+    text = lab3a.return_text_value()
+    print(text)
+    print(lab3a.return_number_value())
